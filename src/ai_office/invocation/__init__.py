1e3614293e05145f92044154b8f1ebc06cea884b
TOSHIO <solution92001@gmail.com>
2026-07-25 22:37:04 +0900

    Add normalized model invocation results

--- src/ai_office/invocation/__init__.py
+++ src/ai_office/invocation/__init__.py
@@ -4,5 +4,18 @@ from ai_office.invocation.model_invocation_request import (
     ModelInvocationRequest,
     build_model_invocation_request,
 )
+from ai_office.invocation.model_invocation_result import (
+    ModelInvocationFailure,
+    ModelInvocationFailureCategory,
+    ModelInvocationResult,
+    ModelInvocationSuccess,
+)
 
-__all__ = ["ModelInvocationRequest", "build_model_invocation_request"]
+__all__ = [
+    "ModelInvocationFailure",
+    "ModelInvocationFailureCategory",
+    "ModelInvocationRequest",
+    "ModelInvocationResult",
+    "ModelInvocationSuccess",
+    "build_model_invocation_request",
+]
